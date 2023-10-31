385330d3f8b8b23984ddaa67f72aa1019edcd66b
Somesh Vemula <ratansomesh@gmail.com>
2023-10-31 07:51:14 -0400

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Button>Click me!</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="orange.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
