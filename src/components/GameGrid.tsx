4e2fe863417137b933918bc2260b6ab3131d877a
Somesh Vemula <ratansomesh@gmail.com>
2023-11-03 13:25:31 -0400

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,32 @@
-import { Grid, Progress, SimpleGrid, Text } from "@chakra-ui/react";
+import { Grid, Progress, SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import Lottie from "lottie-react";
 import LoadingAnimation from "../assets/LoadingAnimation.json";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
+  const skeletonMap = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
   if (error) {
     return <Text>{error}</Text>;
   }
-  if (isLoading) {
-    return <Lottie animationData={LoadingAnimation} />;
-  }
+
   return (
     <>
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        gap={3}
-        marginLeft={5}
         padding={"10px"}
+        spacing={3}
       >
-        {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
-        ))}
+        {isLoading &&
+          skeletonMap.map((Skeleton) => (
+            <GameCardSkeleton key={Skeleton}></GameCardSkeleton>
+          ))}
+        {!isLoading &&
+          games.map((game) => <GameCard key={game.id} game={game}></GameCard>)}
       </SimpleGrid>
     </>
   );
